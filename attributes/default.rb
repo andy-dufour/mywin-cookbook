45f2a50d1c4bd179121b7a2c622bb217bab28960
Alex Pop <al3xpop@gmail.com>
2014-11-07 19:39:16 +0000

    test-kitchen working

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
-
 default['notepadpp']['url'] = 'http://download.tuxfamily.org/notepadplus/6.6.8/npp.6.6.8.Installer.exe'
 # checksum obtained by running: sha256sum npp.6.6.8.Installer.exe
 default['notepadpp']['checksum'] = '24809a80df9201706f351abfdd09bc45ef404bd3f2af98994ab2d571ccc86c53'
