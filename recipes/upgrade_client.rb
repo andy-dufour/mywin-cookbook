66ed99d7cd8444fbe8497670d5b0b96e62c0a9b6
Alex Pop <al3xpop@gmail.com>
2015-04-14 23:18:42 +0300

    add comments

--- recipes/upgrade_client.rb
+++ recipes/upgrade_client.rb
@@ -1,4 +1,9 @@
-log "*** Upgrading chef-client!!!!!!"
+#
+# Cookbook Name:: mywin-cookbook
+# Recipe:: upgrade_client
+#
+
+log "*** Upgrading chef-client!"
 
 temp_dir='c:\\temp\\'
 
@@ -6,11 +11,13 @@ directory temp_dir do
   action :create
 end
 
+# download the remote chef-client msi and notify the powershell script
 remote_file "#{temp_dir}chef-client.msi" do
   source node['mywin-cookbook']['chef_package']
   notifies :run, "powershell_script[upgrade-chef-client]"
 end
 
+# only run this script when notified
 powershell_script "upgrade-chef-client" do
   environment ({'TEMP_DIR' => temp_dir})
   code <<-EOH
