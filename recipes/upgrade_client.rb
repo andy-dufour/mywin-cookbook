d94f0f61dd8a4c2ddc4ff3c3d18b1c4d01a9b251
Alex Pop <al3xpop@gmail.com>
2015-04-15 11:24:08 +0300

    current version detection

--- recipes/upgrade_client.rb
+++ recipes/upgrade_client.rb
@@ -3,27 +3,36 @@
 # Recipe:: upgrade_client
 #
 
-log "*** Upgrading chef-client!"
-
-temp_dir='c:\\temp\\'
-
-directory temp_dir do
-  action :create
+chef_current_version = node['chef_packages']['chef']['version']
+chef_desired_version = '0.0.0'
+if (node['mywin-cookbook']['chef_package'] =~ /chef-client-(\d+\.\d+\.\d+).*msi/)
+  chef_desired_version = $1
+else
+  raise "ERROR: Cannot parse version from chef package name"
 end
 
-# download the remote chef-client msi and notify the powershell script
-remote_file "#{temp_dir}chef-client.msi" do
-  source node['mywin-cookbook']['chef_package']
-  notifies :run, "powershell_script[upgrade-chef-client]"
-end
-
-# only run this script when notified
-powershell_script "upgrade-chef-client" do
-  environment ({'TEMP_DIR' => temp_dir})
-  code <<-EOH
-    $log = "${ENV:TEMP_DIR}chef_upgrade.txt"
-    $dest_inst = "${ENV:TEMP_DIR}chef-client.msi"
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $dest_inst ADDLOCAL=ChefClientFeature,ChefServiceFeature /norestart /L*v+ $log"
-  EOH
-  action :nothing
+temp_dir='c:/chef_temp'
+if (chef_current_version != chef_desired_version)
+  log "Upgrading chef-client #{chef_current_version} to #{chef_desired_version}"
+  directory temp_dir do
+    action :create
+  end
+  remote_file "#{temp_dir}/chef-client.msi" do
+    source node['mywin-cookbook']['chef_package']
+    notifies :run, "powershell_script[upgrade-chef-client]", :delayed
+  end
+  powershell_script "upgrade-chef-client" do
+    environment ({'TEMP_DIR' => temp_dir})
+    code <<-EOH
+      $log = "${ENV:TEMP_DIR}/chef_upgrade.txt"
+      $dest_inst = "${ENV:TEMP_DIR}/chef-client.msi"
+      Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $dest_inst ADDLOCAL=ChefClientFeature,ChefServiceFeature /norestart /L*v+ $log"
+    EOH
+    action :nothing
+  end
+else
+  log "chef-client #{chef_current_version} is up-to-date"
+  directory temp_dir do
+    action :delete
+  end
 end
